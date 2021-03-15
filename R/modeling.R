fa3ef1269cf655dfaf9bb73694118218cdf98bb9
despresj <despresj@mail.gvsu.edu>
2021-03-15 15:18:23 -0400

    prepped for modeling

--- R/modeling.R
+++ R/modeling.R
@@ -12,7 +12,7 @@ preds <- merged %>%
   names()
 
 varcombiner <- function(vars, outcome){
-  models <- vector(mode = "list", length = 2^length(preds)-1)
+  models <- list()
   for (i in 1:length(vars)) {
     vc <- combn(vars,i)
     for (j in 1:ncol(vc)) {
@@ -37,56 +37,8 @@ best_subsets_model <- map(models, logistic) %>%
   distinct()
 
 best_subsets_model %>% 
-  readr::write_csv("data", "bss.csv")
+  readr::write_csv(here::here("data", "bss.csv"))
 
 
 # predictions -------------------------------------------------------------
-
-
-formula <- merged %>% 
-  select_if(is.numeric) %>% 
-  colnames() %>% 
-  # random for now
-  sample(5) %>% 
-  paste(collapse = " + ") %>% 
-  paste("win ~", .)
-
-formula
-
-logistic_fit <- glm(formula = formula, data = merged,
-                    family = binomial(link = logit))
-
-modeltibble <- logistic_fit %>% 
-  tidy(conf.int = TRUE) %>% 
-  mutate(`exp(estimate)` = exp(estimate)) %>% 
-  relocate(`exp(estimate)`, .after = estimate) %>% 
-  mutate_if(is.numeric, round, 5)
-
-pred <- modeltibble %>% 
-  select(term) %>% 
-  filter(term != "(Intercept)") %>% 
-  bind_cols(pred = c(5, 3, 2, 4,5)) %>% 
-  pivot_wider(names_from = term, values_from = pred)
-
-
-predict.glm(logistic_fit, data.frame(pred), type = "response")
-
-# TODO: we need a function that takes inputed game, and outputs a preiction
-
-logistic_fit
-
-
-predictor <- function (object, modeltibble, predvec) {
-  tib <- modeltibble[1]
-  tibs <- tib[2:6,]
-  tibs <- cbind(tibs, predvec)
-  reshape(tibs, direction = "wide")
-  return(tibs)
-}
-
-modeltibble %>% 
-  select(term)
-debugonce(predictor)
-predictor(logistic_fit, modeltibble = modeltibble, predvec = c(5, 3, 2, 4,5))
-
 
