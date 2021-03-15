e44167d6fc83e4d7f59be000e015d0e11f9b266b
despresj <despresj@mail.gvsu.edu>
2021-03-15 15:18:07 -0400

    cleaning bracket

--- R/bracket.R
+++ R/bracket.R
@@ -7,12 +7,14 @@ bracket <- bracket %>%
   
 colnames(bracket) <- c("x1", "x2")
 
-
 stack(bracket) %>% 
   select(values) %>% 
   drop_na() %>% 
   tibble() %>% 
+  rename(team = values) %>% 
   mutate(game = 1:n(),
          game = as.numeric(game),
-         game = if_else(game %% 2 == 1, game + 1, game),
-        team = values) 
+         otherteam  = if_else(game %% 2 == 1, team, lag(team)),
+         game = if_else(game %% 2 == 1, game + 1, game), .before = team) %>% 
+  filter(team != otherteam)
+  
