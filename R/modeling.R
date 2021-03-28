c12c85c2c7495545e6befe99657e53877686fc64
despresj <despresj@mail.gvsu.edu>
2021-03-28 13:32:52 -0400

    wrote function to predict outcome

--- R/modeling.R
+++ R/modeling.R
@@ -5,7 +5,6 @@ source(here::here("R", 'cleaningScript.R'))
 
 # merged <- readr::read_csv(here::here("data", "merged.csv"))
 
-
 # Take a look -------------------------------------------------------------
 
 
@@ -34,28 +33,55 @@ fit <- glm(win ~ x3fg +
            data = merged, family = "binomial")
 summary(fit)
 
+
 s2021 <- team_stats %>% 
   filter(season == 2021, 
          TeamID %in% c(bracket$teamid, bracket$otherteamid)) %>% 
-  distinct(TeamID,.keep_all = TRUE )
-
-# TODO: Need a predictor function.
-
-preddat <- merged %>% 
-  filter(season == 2020, 
-         team %in% c(bracket$team, bracket$otherteam)) %>% 
+  distinct(TeamID,.keep_all = TRUE ) %>% 
+  select(-season) %>% 
   select(
+    team,
+    TeamID,
     x3fg,
-    opposingx3fg,
     fg_percent,
-    opposingfg_percent,
     ft_percent,
-    opposingft_percent,
     rpg,
-    opposingrpg,
     st,
-    opposingst,
     to,
-    opposingto,
-    opposingbkpg,
     bkpg)
+
+s2021
+
+predictor_fn <- function (team_1_id, team_2_id) {
+  stats_team_1 <- filter(s2021, TeamID == team_1_id)
+  stats_team_2 <- filter(s2021, TeamID == team_2_id) %>% 
+  rename_all(~ paste0("opposing",.)) 
+  bound_cols <- bind_cols(stats_team_1, stats_team_2)
+  pred <- predict(fit, newdata = bound_cols, type = "response")
+  return(pred)
+}
+
+predictor_fn(team_1 = teams_in_turn[1], team_2 = teams_in_turn[2])
+
+bracket_ <- bracket %>% 
+  # missing app st. ID 1111
+  filter(team != "app st.")
+
+probs <- map2_dbl(bracket_$teamid, bracket_$otherteamid, predictor_fn)
+# app state is missing so I am assigning it a 0 pob of winning.
+probs <- c(probs[1:16], "0", probs[17:33])
+
+bracket <- bracket %>% 
+  mutate(team_prob = as.numeric(probs),
+         otherteam_prob = 1 - team_prob, 
+         game = paste0(team, " vs. ", otherteam),
+         predicted_winner = if_else(team_prob > 0.5, team, otherteam),
+         predicted_winnerid = if_else(team_prob > 0.5, teamid, otherteamid))
+  
+games <- bracket %>% 
+  mutate(team_prob = as.numeric(probs),
+         otherteam_prob = 1 - team_prob, 
+         game = paste0(team, " vs. ", otherteam)) %>% 
+  select(game, team_prob, otherteam_prob)
+
+games
