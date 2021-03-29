03c43834ea9fb253510ebd858829a0fc5c053bac
despresj <despresj@mail.gvsu.edu>
2021-03-29 14:32:56 -0400

    re-ran model on bracket without pockets

--- R/modeling.R
+++ R/modeling.R
@@ -33,9 +33,11 @@ fit <- glm(win ~ x3fg +
            data = merged, family = "binomial")
 summary(fit)
 
+begining_bracket
+
 s2021 <- team_stats %>% 
   filter(season == 2021, 
-         TeamID %in% c(bracket$teamid, bracket$otherteamid)) %>% 
+         TeamID %in% c(begining_bracket$teamid, begining_bracket$otherteamid)) %>% 
   distinct(TeamID, .keep_all = TRUE) %>% 
   select(-season) 
 
@@ -50,26 +52,29 @@ predictor_fn <- function (team_1_id, team_2_id) {
   return(pred)
 }
 
-possibly_predictor_fn <- possibly(.f = predictor_fn, otherwise = 0)
+possibly_predictor_fn <- possibly(.f = predictor_fn, otherwise = "ERROR")
 
 # app state is missing from the data
 # that 0 puts a zero pros of them winning to effectively drop them.
-
-probs <- map2_dbl(.x = bracket$teamid, 
-                  .y = bracket$otherteamid, 
+begining_bracket
+probs <- map2_dbl(.x = begining_bracket$teamid, 
+                  .y = begining_bracket$otherteamid, 
                   .f = possibly_predictor_fn)
 
-bracket <- bracket %>% 
+probs
+
+begining_bracket <- begining_bracket%>% 
   mutate(team_prob = probs,
          otherteam_prob = 1 - team_prob, 
          game = paste0(team, " vs. ", otherteam),
          predicted_winner = if_else(team_prob > 0.5, team, otherteam),
          predicted_winnerid = if_else(team_prob > 0.5, teamid, otherteamid))
   
-games <- bracket %>% 
+games <- begining_bracket %>% 
   mutate(team_prob = as.numeric(probs),
          otherteam_prob = 1 - team_prob, 
          game = paste0(team, " vs. ", otherteam)) %>% 
   select(game, team_prob, otherteam_prob)
 
 games
+
