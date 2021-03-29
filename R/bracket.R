28dc2a1b358e028eb12f3829cf4d4f277268b386
despresj <despresj@mail.gvsu.edu>
2021-03-29 14:20:08 -0400

    getting bracket without pockets

--- R/bracket.R
+++ R/bracket.R
@@ -5,6 +5,9 @@ library(tidyverse)
 team_names <- readr::read_csv(here::here("rawdata", "MTeamSpellings.csv"))
 bracket <- readxl::read_excel(here::here("data", "prebracket.xlsx"))
 
+begining_bracket <- readxl::read_excel(here::here("data", "begining_bracket.xlsx"))
+
+
 # Even better
 # bracket <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1zaWZ2Xh7sll-PZKA1DhKvS6TdfE0Yggk0m5OWY4aICc/edit#gid=0")
 # remember API times out...
@@ -59,3 +62,27 @@ bracket <- bracket %>%
 
 bracket
 
+
+# begining bracket (no pockets) -------------------------------------------
+
+begining_bracket <- begining_bracket %>% 
+  select(2, 35) %>% 
+  stack() %>% 
+  drop_na() %>% 
+  tibble() %>% 
+  rename(team = values) %>% 
+  mutate(team = tolower(team)) %>% 
+  mutate(game = 1:n(),
+         game = as.numeric(game),
+         otherteam  = if_else(game %% 2 == 1, team, lag(team)),
+         game = if_else(game %% 2 == 1, game + 1, game), .before = team) %>% 
+  filter(team != otherteam) %>% 
+  filter(otherteam != "first round") %>% 
+  left_join(team_names, by = c("team" = "TeamNameSpelling")) %>% 
+  rename(teamid = TeamID) %>% 
+  left_join(team_names, by = c("otherteam" = "TeamNameSpelling")) %>%
+  rename(otherteamid = TeamID) %>% 
+  mutate(teamid = replace(teamid, team == "app st.", 1111)) %>% 
+  mutate_if(is.numeric, as.character) %>% 
+  select(-ind, -game)
+  
