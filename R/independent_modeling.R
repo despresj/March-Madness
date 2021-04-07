5c4274c4f642b0bf2b17a6e25d7abdbfa1cff51d
despresj <despresj@mail.gvsu.edu>
2021-04-07 16:00:35 -0400

    cleaned up script and and re-ran models

--- R/independent_modeling.R
+++ R/independent_modeling.R
@@ -12,7 +12,6 @@ options(tibble.print_min = 20)
 # TODO: recipes::step_normalize() try that see if there is a difference
 
 # logsitic fit ------------------------------------------------------------
-
 # logistic_fit <- glm(win ~ x3fg +
 #                       opposingx3fg +
 #                       # field goal pct
@@ -34,26 +33,26 @@ options(tibble.print_min = 20)
 #                       opposingbkpg +
 #                       bkpg,
 #                     data = merged, family = "binomial")
-# 
+# # 
 # start <- Sys.time()
 # logistic_predictions <- ids %>%
 #   mutate(predicted_probs = map2_dbl(.x = ids$teamid,
 #                                     .y = ids$otherid,
-#                                     .f = logistic_predictor))
+#                                     .f = logistic_predictor)) %>%
+# # This takes about 30 seconds to run..not if we cache it 
+#   mutate(
+#     predicted_winner = if_else(predicted_probs > .5, team, other_team),
+#     predicted_winner_probs = if_else(predicted_probs > .5, predicted_probs , 1 - predicted_probs),
+#     predicted_loser_probs = 1 - predicted_winner_probs,
+#     prediction_points = predicted_winner_probs - predicted_loser_probs,
+#     .after = game
+#   )
 # paste0("runtime: ", round(Sys.time() - start, 2))
-# # This takes about 30 seconds to run..not if we cache it
+
 # saveRDS(logistic_predictions, here::here("cache", "logistic_predictions.RDS"))
 
-logistic_predictions <- 
-  readRDS(here::here("cache", "logistic_predictions.RDS")) %>% 
-  mutate(  
-    predicted_winner = if_else(predicted_probs > .5, team, other_team),
-    predicted_winner_probs = if_else(predicted_probs > .5, predicted_probs , 1 - predicted_probs),
-    predicted_loser_probs = 1 - predicted_winner_probs,
-    prediction_points = predicted_winner_probs - predicted_loser_probs,
-    .after = game
-  )
 
+logistic_predictions <- readRDS(here::here("cache", "logistic_predictions.RDS"))
 logistic_predictions
 
 
@@ -111,7 +110,6 @@ logistic_predictions
 # saveRDS(posson_prediction, here::here("cache", "posson_prediction.RDS"))
 
 posson_prediction <- readRDS(here::here("cache", "posson_prediction.RDS"))
-
 posson_prediction
 
 # mulitnom ----------------------------------------------------------------
@@ -137,14 +135,14 @@ posson_prediction
 #                  opposingbkpg +
 #                  bkpg, family = VGAM::cumulative(parallel=TRUE),
 #                  data = merged)
-#   
+# 
 # multinom_predictor_fn <- function (team_1_id, team_2_id) {
 #   stats_team_1 <- filter(s2021, TeamID == team_1_id)
 #   stats_team_2 <- filter(s2021, TeamID == team_2_id) %>%
 #   rename_all(~ paste0("opposing",.))
 #   bound_cols <- bind_cols(stats_team_1, stats_team_2)
 #   pred <- VGAM::predict(fit_multinom, newdata = bound_cols, type = "response")
-#   
+# 
 #   return(pred)
 # }
 # 
@@ -163,24 +161,24 @@ posson_prediction
 # # this takes about a 160 secs to run
 # paste0("runtime: ", round(Sys.time() - start, 2))
 # 
+# add_name <- function(x){
+#   names(x) <- c("<-12", "-12:-4","-4:4", "4:13", ">13")
+#   return(x)
+# }
+# 
+# add_name_other <- function(x){
+#   names <- c("<-12", "-12:-4","-4:4", "4:13", ">13")
+#   names(x) <- paste0("other", names)
+#   return(x)
+# }
+# 
+# multinom_prediction <- multinom_prediction  %>% 
+#   mutate(predicted_score = map(predicted_score, add_name)) %>% 
+#   unnest_wider(predicted_score) %>% 
+#   mutate(other_predicted_score = map(other_predicted_score, add_name_other)) %>% 
+#   unnest_wider(other_predicted_score) 
+#   
 # saveRDS(multinom_prediction, here::here("cache", "multinom_prediction.RDS"))
 
 multinom_prediction <- readRDS(here::here("cache", "multinom_prediction.RDS"))
-
-add_name <- function(x){
-  names(x) <- c("<-12", "-12:-4","-4:4", "4:13", ">13")
-  return(x)
-}
-
-add_name_other <- function(x){
-  names <- c("<-12", "-12:-4","-4:4", "4:13", ">13")
-  names(x) <- paste0("other", names)
-  return(x)
-}
-
-multinom_prediction  %>% 
-  mutate(predicted_score = map(predicted_score, add_name)) %>% 
-  unnest_wider(predicted_score) %>% 
-  mutate(other_predicted_score = map(other_predicted_score, add_name_other)) %>% 
-  unnest_wider(other_predicted_score) 
-  
+multinom_prediction
