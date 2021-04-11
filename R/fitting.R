572a05703c5700fe3946c3181a37245cefea84d4
despresj <despresj@mail.gvsu.edu>
2021-04-11 11:45:58 -0400

    worked on slides

---
+++ R/fitting.R
@@ -0,0 +1,65 @@
+logistic_fit <- glm(win ~ x3fg +
+                       opposingx3fg +
+                       # field goal pct
+                       fg_percent +
+                       opposingfg_percent +
+                       # free throws
+                       ft_percent +
+                       opposingft_percent +
+                       # rebound per game
+                       rpg +
+                       opposingrpg +
+                       # steels
+                       st +
+                       opposingst +
+                       #turnover
+                       to +
+                       opposingto +
+                       # blocks
+                       opposingbkpg +
+                       bkpg,
+                     data = merged, family = "binomial")
+ 
+ fit_poisson <- glm(team_score ~ x3fg +
+              opposingx3fg +
+              # field goal pct
+              fg_percent +
+              opposingfg_percent +
+              # free throws
+              ft_percent +
+              opposingft_percent +
+              # rebound per game
+              rpg +
+              opposingrpg +
+              # steels
+              st +
+              opposingst +
+              #turnover
+              to +
+              opposingto +
+              # blocks
+              opposingbkpg +
+              bkpg,
+              data = merged, family = poisson(link = "log"))
+ 
+ fit_multinom <- VGAM::vglm(x_tile ~ x3fg +
+                  opposingx3fg +
+                  # field goal pct
+                  fg_percent +
+                  opposingfg_percent +
+                  # free throws
+                  ft_percent +
+                  opposingft_percent +
+                  # rebound per game
+                  rpg +
+                  opposingrpg +
+                  # steels
+                  st +
+                  opposingst +
+                  #turnover
+                  to +
+                  opposingto +
+                  # blocks
+                  opposingbkpg +
+                  bkpg, family = VGAM::cumulative(parallel=TRUE),
+                  data = merged)
